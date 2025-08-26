f5aaa469be4e7ba4f83121c51e829ce6a47a32c0
Kirill L <lobachevsky.kerya@gmail.com>
2025-08-26 22:32:33 +0300

    added filter and search feature (1 field without params in url)

--- src/app/titles/searchBlock/searchByDemographic/SearchByDemographic.tsx
+++ src/app/titles/searchBlock/searchByDemographic/SearchByDemographic.tsx
@@ -1,18 +1,38 @@
-import { SelectField } from '@/components/ui/fields/selectField/SelectField'
+import { ISelectOption, SelectField } from '@/components/ui/fields/selectField/SelectField'
 
-interface Props {}
+import { useSearchStore } from '@/store/search.store'
 
-const options = [
-	{ value: 'chocolate', label: 'Chocolate' },
-	{ value: 'strawberry', label: 'Strawberry' },
-	{ value: 'vanilla', label: 'Vanilla' }
+import { PublicationDemographic } from '@/types/enums'
+
+const demographicOptions = [
+	{ value: PublicationDemographic.SHOUNEN, label: 'Сёнэн (Юноши 12-18)' },
+	{ value: PublicationDemographic.SHOUJO, label: 'Сёдзё (Девушки 12-18)' },
+	{ value: PublicationDemographic.JOSEI, label: 'Дзёсэй (Мужчины 18-35)' },
+	{ value: PublicationDemographic.SEINEN, label: 'Сэйнэн (Женщины 18-35)' }
 ]
 
-export function SearchByDemographic({}: Props) {
+export function SearchByDemographic() {
+	const { selectedDemographics, setSelectedDemographics, appliedDemographics } = useSearchStore()
+
+	const handleChange = (selectedOptions: readonly ISelectOption[] | null) => {
+		const newSelectedValues = selectedOptions ? selectedOptions.map(option => option.value) : []
+
+		setSelectedDemographics(newSelectedValues)
+	}
+
+	const selectedOptionsForSelect = demographicOptions.filter(option =>
+		selectedDemographics.includes(option.value)
+	)
+
 	return (
 		<div>
-			<p></p>
-			<SelectField options={options} placeholder='Выбрать возрастную группу' />
+			<p className='mb-[1rem]'>Возрастная группа</p>
+			<SelectField
+				options={demographicOptions}
+				placeholder='Любая'
+				value={selectedOptionsForSelect}
+				onChange={handleChange}
+			/>
 		</div>
 	)
 }
